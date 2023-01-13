2b2fbbb55d5341749b00b784d0e7a2625dc3e5da
XsidX <developer.sid@outlook.com>
2023-01-13 14:39:06 +0300

    GraphQL AppSync API- retrieve and display posts

--- configureAmplify.js
+++ configureAmplify.js
@@ -1,4 +1,4 @@
-import Amplify from "aws-amplify";
-import config from ".src/aws-exports";
+import {Amplify} from "aws-amplify";
+import config from "./src/aws-exports";
 
 Amplify.configure(config);
