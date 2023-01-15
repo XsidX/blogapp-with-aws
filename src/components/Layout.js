22b0a65cf569954941a5aa8e67f68a73f1f85cb7
XsidX <developer.sid@outlook.com>
2023-01-15 10:02:07 +0300

    Setup Navbar component

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  )
+}
+
+export default Layout
