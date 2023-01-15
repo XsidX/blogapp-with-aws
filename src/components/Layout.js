783124f68652b801ceb5158d8ff36631e46b10a8
XsidX <developer.sid@outlook.com>
2023-01-15 11:23:57 +0300

    Setup profile, sign in and sign up pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,9 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
-      {children}
+      <div className="md:max-w-4xl px-4 md:mx-auto border border-gray-100">
+        {children}
+      </div>
     </div>
   )
 }
