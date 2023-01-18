477e8f215887ccb82a01e63113485ca431966324
XsidX <developer.sid@outlook.com>
2023-01-18 15:47:59 +0300

    Update post image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <div className="md:max-w-4xl px-4 md:mx-auto border border-gray-100">
+      <div className="md:max-w-4xl px-4 pb-6 md:mx-auto">
         {children}
       </div>
     </div>
