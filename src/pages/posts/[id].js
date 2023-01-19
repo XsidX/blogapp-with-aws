eae543a38cd0fbce2a2e71d509f55f6f1c213074
XsidX <developer.sid@outlook.com>
2023-01-19 16:58:04 +0300

    Create comments in post vew, save to db

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -3,9 +3,10 @@ import Image from 'next/image'
 import { API, Storage } from 'aws-amplify'
 import { useRouter } from 'next/router'
 import ReactMarkdown from 'react-markdown'
+import { v4 as uuid } from 'uuid'
 import '../../../configureAmplify'
 import { listPosts, getPost } from '../../graphql/queries'
-import { createComment } from '../../graphql/mutations'
+import { createComment as createCommentMutation } from '../../graphql/mutations'
 import dynamic from 'next/dynamic'
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
 import 'easymde/dist/easymde.min.css'
@@ -15,8 +16,11 @@ const initialState = { message: '' }
 
 const Post = ({ post }) => {
   const [coverImage, setCoverImage] = useState(null)
+
   const [comment, setComment] = useState(initialState)
   const [showComments, setShowComments] = useState(false)
+
+  const { message } = comment
 
   const toggleComments = () => setShowComments(!showComments)
 
@@ -36,6 +40,24 @@ const Post = ({ post }) => {
     return <div>Loading...</div>
   }
 
+  const createComment = async () => {
+    if (!message) return
+    const id = uuid()
+    comment.id = id
+
+    try{
+    await API.graphql({
+      query: createCommentMutation,
+      variables: { input: comment },
+      authMode: 'AMAZON_COGNITO_USER_POOLS',
+    })
+    } catch (err) {
+      console.log('error creating comment:', err)
+    }
+
+    router.push(`/posts/${post.id}`)
+  }
+  
   return (
     <div>
       <h1 className="text-2xl font-semibold tracking-wide mt-6">{post.title}</h1>
@@ -56,6 +78,28 @@ const Post = ({ post }) => {
       <div className="mt-4 text-base leading-relaxed text-left break-words whitespace-pre-line overflow-hidden">
         <ReactMarkdown>{post.content}</ReactMarkdown>
       </div>
+      {/* comments */}
+      <div className="mt-4">
+        <h2 className="text-xl font-semibold tracking-wide">Comments</h2>
+        {
+          post.comments.items && post.comments.items.map(comment => (
+            <div key={comment.id} className="mt-4 space-y-2">
+              <p className="text-gray-700 text-sm">
+                {comment.message} - {comment.username}
+              </p>
+              <p className="text-gray-500 text-xs">
+                {new Date(comment.createdAt).toDateString()}
+              </p>
+            </div>
+          ))
+        }
+
+        {
+          post.comments.items.length === 0 && (
+            <p className="mt-4">No comments yet</p>
+          )
+        }
+      </div>
 
       <div className="mt-4">
         <button
@@ -67,13 +111,15 @@ const Post = ({ post }) => {
         {showComments && (
           <div className="mt-4">
             <SimpleMDE
-              value='hello'
+              value={message}
+              onChange={value => setComment({ ...comment, message: value, postId: post.id })}
             />
             <button
+              type='button'
               className="mt-2 font-semibold text-blue-500"
               onClick={() => createComment({ variables: { input: comment } })}
             >
-              Comment
+              Save
             </button>
           </div>
         )}
