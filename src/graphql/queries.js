fcda3baa2e26266127e4a2bc9e9c90ab4e426d78
XsidX <developer.sid@outlook.com>
2023-01-19 15:53:12 +0300

    Setup comments rel, Create comments in amplify console

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -9,6 +9,17 @@ export const getPost = /* GraphQL */ `
       content
       username
       coverImage
+      comments {
+        items {
+          id
+          message
+          postId
+          createdAt
+          updatedAt
+          createdBy
+        }
+        nextToken
+      }
       createdAt
       updatedAt
     }
@@ -27,6 +38,9 @@ export const listPosts = /* GraphQL */ `
         content
         username
         coverImage
+        comments {
+          nextToken
+        }
         createdAt
         updatedAt
       }
@@ -55,8 +69,99 @@ export const postsByUsername = /* GraphQL */ `
         content
         username
         coverImage
+        comments {
+          nextToken
+        }
         createdAt
         updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getComment = /* GraphQL */ `
+  query GetComment($id: ID!) {
+    getComment(id: $id) {
+      id
+      message
+      post {
+        id
+        title
+        content
+        username
+        coverImage
+        comments {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      postId
+      createdAt
+      updatedAt
+      createdBy
+    }
+  }
+`;
+export const listComments = /* GraphQL */ `
+  query ListComments(
+    $filter: ModelCommentFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listComments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        message
+        post {
+          id
+          title
+          content
+          username
+          coverImage
+          createdAt
+          updatedAt
+        }
+        postId
+        createdAt
+        updatedAt
+        createdBy
+      }
+      nextToken
+    }
+  }
+`;
+export const commentsByPostId = /* GraphQL */ `
+  query CommentsByPostId(
+    $postId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelCommentFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    commentsByPostId(
+      postId: $postId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        message
+        post {
+          id
+          title
+          content
+          username
+          coverImage
+          createdAt
+          updatedAt
+        }
+        postId
+        createdAt
+        updatedAt
+        createdBy
       }
       nextToken
     }
